6dfc33659f6e4f6f5cf2d1ee51897155bee78bd9
whalepopov <popovaa07@gmail.com>
2022-02-13 13:31:54 +0300

    test all

--- src/test/java/IndexStorageTest.java
+++ src/test/java/IndexStorageTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class IndexStorageTest {
@@ -29,5 +31,43 @@ public class IndexStorageTest {
         expected = arrayIndexStorage.reverse();
 
         Assertions.assertEquals(Arrays.toString(actual), Arrays.toString(expected));
+
+        try {
+            int actualInt = arrayIndexStorage.get(-1313);
+        } catch (IndexOutOfBoundsException thrown) {
+            Assertions.assertEquals("Ячейка не существет!", thrown.getMessage());
+        }
+
+        try {
+            int actualInt = arrayIndexStorage.get(1313);
+        } catch (IndexOutOfBoundsException thrown) {
+            Assertions.assertEquals("Ячейка не существет!", thrown.getMessage());
+        }
+
     }
+
+    @Test
+    public void getnull() throws IOException {
+        IndexStorage indexStorage = new IndexStorage(2);
+        try {
+            int actual = indexStorage.get(13);
+        } catch (IndexOutOfBoundsException thrown) {
+            Assertions.assertEquals("Ячейка не существет!", thrown.getMessage());
+        }
+
+        try {
+            int actual = indexStorage.get(-13);
+        } catch (IndexOutOfBoundsException thrown) {
+            Assertions.assertEquals("Ячейка не существет!", thrown.getMessage());
+        }
+
+        indexStorage = new IndexStorage(-13);
+        try {
+            int actual = indexStorage.size();
+        } catch (IllegalArgumentException thrown) {
+            Assertions.assertEquals("Размер массива должен быть больше нуля!", thrown.getMessage());
+        }
+
+    }
+
 }
